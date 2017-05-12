490e393f739a52b653739be70753bfb6a1a3e4a2
Adam Bosco <adam@avocaweb.net>
2017-05-12 13:57:39 -0700

    Unify /color validation conditions and error message

--- js/messages.js
+++ js/messages.js
@@ -93,12 +93,13 @@ MessageList.prototype.handleSlashCommand = function(str) {
 		}
 	} else if (components[0] == 'color') {
 		if (components[1]) {
-			if (components[1].length == 1){
+			if(components[1].length == 1 && /^[a-z0-5]$/i.test(components[1])) {
 				var color = components[1];
 				settings.setColor(color);
 				this.write('Set chat color to "' + color + '". Sample: "' + colorCallback(null, color, 'foo bar baz') + '"');
-			} else {
-				this.write("Please specify a one-character color code.")
+			}
+			else {
+				this.write("Invalid color code. Please specify a single letter, or a number in the range 0-5.")
 			}
 		} else {
 			if (settings.color_code) {
